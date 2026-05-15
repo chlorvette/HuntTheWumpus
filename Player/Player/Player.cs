b91a8787a90044695e4bbfa6a28b2320ef352e9f
Prisha3Khanna <2002421@apps.nsd.org>
2026-05-15 15:03:08 -0700

    Updated methods

--- Player/Player/Player.cs
+++ Player/Player/Player.cs
@@ -6,69 +6,101 @@
         public int GoldCoins { get; set; }
         public int Turns { get; set; }
         public int EndingScore { get; set; }
+        public bool IsAlive { get; private set; } = true; 
 
-        public Player() 
+        public Player()
         {
-            Arrows = 3; 
-            GoldCoins = 0;
-            Turns = 0;
-            EndingScore = 0;
+            PlayerManager playerManager = new PlayerManager();
+            Arrows = playerManager.Arrows;
+            GoldCoins = playerManager.GoldCoins;
+            Turns = playerManager.Turns;
+            EndingScore = playerManager.EndingScore;
         }
+
         public int TakeTurns()
         {
+            if (!IsAlive)
+            {
+                Console.WriteLine("You lost");
+                Score();
+                return Turns;
+            }
             Turns++;
             return Turns;
-
         }
-        public int ShootArrow()
+
+        public bool ShootArrow()
         {
             if (Arrows > 0)
             {
                 Arrows--;
-                return Arrows;
+                return true;
             }
-            return Arrows;
-               
+            if (!IsAlive)
+            {
+                return false;
+            } 
+            else
+            {
+                Console.WriteLine("You lost");
+                IsAlive = false;
+                Score();
+                return false;
+            }
         }
 
-        public (int, int) GainArrow(bool PassTrivia)
+        public bool GainArrow()
         {
-            if (PassTrivia) // Pretend method for Trivia
+            if (!IsAlive) return false;
+
+            if (Arrows > 0)
             {
                 Arrows++;
                 GoldCoins--;
-                return (Arrows, GoldCoins);
-  
+                return true;
             }
             else
             {
-                // else lose Triva then won't gain arrrow
-                Arrows--;
-                return (Arrows, GoldCoins);
+                Console.WriteLine("You lost");
+                IsAlive = false;
+                Score();
+                return false;
             }
         }
-
 
         public bool EncounterWumpus()
         {
             Arrows--;
-            return false;
+            if (!IsAlive)
+            {
+                return false;
+            }
+            
+            if (Arrows < 0)
+            {
+                Console.WriteLine("You lost");
+                IsAlive = false;
+                Score();
+                return false;
+            }
+            return true;
         }
+
         public int Score()
         {
-            EndingScore = (GoldCoins * 10) - (Turns * 1);
+            // Ensures score never drops below 0
+            EndingScore = Math.Max(0, (GoldCoins * 10) - (Turns * 1));
             return EndingScore;
         }
-        public void DisplayRepository() 
+
+        public void Display()
         {
             Console.WriteLine($"Arrows: {Arrows}");
             Console.WriteLine($"Gold Coins: {GoldCoins}");
             Console.WriteLine($"Turns: {Turns}");
             Console.WriteLine($"Ending Score: {EndingScore}");
+            Console.WriteLine($"Status: {(IsAlive ? "Alive" : "Dead")}");
         }
-       
-           
     }
 
-   
 }
--- Player/Player/PlayerManager.cs
+++ Player/Player/PlayerManager.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Player
 {
-    internal class PlayerManager
+    public class PlayerManager
     {
+        public int Arrows { get; set; }
+        public int GoldCoins { get; set; }
+        public int Turns { get; set; }
+        public int EndingScore { get; set; }
+
+        public PlayerManager()
+        {
+            Arrows = 3;
+            GoldCoins = 0;
+            Turns = 0;
+            EndingScore = 0;    
+
+        }
+        public PlayerManager(int arrows, int goldCoins, int turns, int endingScore)
+        {
+            Arrows = arrows;
+            GoldCoins = goldCoins;
+            Turns = turns;
+            EndingScore = endingScore;
+        }
+
     }
 }
