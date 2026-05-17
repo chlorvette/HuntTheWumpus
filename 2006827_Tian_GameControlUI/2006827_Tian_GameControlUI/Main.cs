0aee2f0b71e263ddacb8279d4ec0e9be8f1788ae
chloe <91811945+chlorvette@users.noreply.github.com>
2026-05-17 13:23:33 -0700

    remove default comments + add new ones

--- 2006827_Tian_GameControlUI/2006827_Tian_GameControlUI/Main.cs
+++ 2006827_Tian_GameControlUI/2006827_Tian_GameControlUI/Main.cs
@@ -17,17 +17,19 @@ namespace _2006827_Tian_GameControlUI
         private AnimatedTexture playerTexture;
         private Tilemap tilemapLayerZero;
         private Tilemap tilemapLayerOne;
-        private const float rotation = 0;
-        private Vector2 playerScale = new Vector2(1f, 1f);
-        private const float depth = 0.5f;
 
+        // player configuration
+        private const float playerRotation = 0;
+        private Vector2 playerScale = new Vector2(1f, 1f);
+        private const float playerDepth = 0.5f;
+
+        // tilemap configuration
         private Vector2 tileScale = new Vector2(2f, 2f);
         private int tilesetChoicesPerType = 12;
         private int tilesetTotalTypes = 13;
         private int tilesetTileDimensions = 16;
         private int tilemapWidthTiles = 25;
         private int tilemapHeightTiles = 15;
-
         private string tilesetKeyPath = @"..\..\..\Content\map\tilesetKey.txt";
         private string tilesetImageName = "map/Dungeon_Tileset";
         private string tilemapsLocation = @"..\..\..\Content\map\";
@@ -37,20 +39,19 @@ namespace _2006827_Tian_GameControlUI
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            playerTexture = new AnimatedTexture(Vector2.Zero, rotation, playerScale, depth);
+            playerTexture = new AnimatedTexture(Vector2.Zero, playerRotation, playerScale, playerDepth);
             tilemapLayerZero = new Tilemap(Content, tilesetImageName, tilemapsLocation+ "tilemapLayer0Template.txt", tilesetTileDimensions, (tilemapHeightTiles, tilemapWidthTiles), tileScale, tilesetKeyPath, tilesetChoicesPerType, tilesetTotalTypes);
             tilemapLayerOne = new Tilemap(Content, tilesetImageName, tilemapsLocation + "tilemapLayer1Template.txt", tilesetTileDimensions, (tilemapHeightTiles, tilemapWidthTiles), tileScale, tilesetKeyPath, tilesetChoicesPerType, tilesetTotalTypes);
-
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         private Viewport viewport;
+
+        // player configuration
         private Vector2 characterPos;
         private SpriteEffects playerSpriteEffect;
         private int moveSpeed = 5;
@@ -58,6 +59,8 @@ namespace _2006827_Tian_GameControlUI
         private const int columns = 11;
         private const int rows = 5;
         private const int framesPerSec = 10;
+
+        // setting initial values
         private bool isMoving = false;
         private bool movingLeft = false;
 
@@ -66,22 +69,24 @@ namespace _2006827_Tian_GameControlUI
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             playerTexture.Load(Content, "ArcherSheet", frames, columns, rows, framesPerSec);
-            playerTexture.Row = 0; // play first row
-            tilemapLayerZero.Load(Content, "map/Dungeon_Tileset");
-            tilemapLayerOne.Load(Content, "map/Dungeon_Tileset");
-            viewport = _graphics.GraphicsDevice.Viewport;
-            characterPos = new Vector2(viewport.Width / 2, viewport.Height / 2);
+            playerTexture.Row = 0; // play first row (idling animation)
 
-            // TODO: use this.Content to load your game content here
+            tilemapLayerZero.Load(Content);
+            tilemapLayerOne.Load(Content);
+
+            viewport = _graphics.GraphicsDevice.Viewport;
+            characterPos = new Vector2((viewport.Width / 2) - (playerTexture.FrameWidth / 2), (viewport.Height / 2) - (playerTexture.FrameHeight / 2));
         }
 
         protected override void Update(GameTime gameTime)
         {
+            // keyboard input => player movement
             KeyboardState keyboardState = Keyboard.GetState();
             if (keyboardState.IsKeyDown(Keys.Back))
             {
                 Exit();
             }
+
             isMoving = false;
             if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
             {
@@ -91,6 +96,7 @@ namespace _2006827_Tian_GameControlUI
                 }
                 isMoving = true;
             }
+
             if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
             {
                 if (characterPos.X > -10)
@@ -100,6 +106,7 @@ namespace _2006827_Tian_GameControlUI
                 isMoving = true;
                 movingLeft = true;
             }
+
             if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
             {
                 if ((characterPos.Y + playerTexture.FrameHeight + moveSpeed) < viewport.Height)
@@ -108,6 +115,7 @@ namespace _2006827_Tian_GameControlUI
                 }
                 isMoving = true;
             }
+
             if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
             {
                 if ((characterPos.X + playerTexture.FrameWidth + moveSpeed - 20) < viewport.Width)
@@ -134,7 +142,6 @@ namespace _2006827_Tian_GameControlUI
                 playerSpriteEffect = SpriteEffects.None;
             }
 
-            // TODO: Add your update logic here
             float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
             playerTexture.UpdateFrame(elapsed);
 
@@ -143,14 +150,14 @@ namespace _2006827_Tian_GameControlUI
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.White);
+            GraphicsDevice.Clear(Color.White); // clear
 
-            // TODO: Add your drawing code here
             _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
             tilemapLayerZero.Draw(_spriteBatch);
             tilemapLayerOne.Draw(_spriteBatch);
             playerTexture.DrawFrame(_spriteBatch, characterPos, playerSpriteEffect);
             _spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
